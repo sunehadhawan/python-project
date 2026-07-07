543222948e06a2aba97795f3963a0325bf419c78
sunehadhawan <anmoldhawan090@gmail.com>
2026-07-06 23:42:23 -0700

    Add files via upload

---
+++ lic.py
@@ -0,0 +1,7 @@
+age=int(input("enter the age of person: "))
+if age>=18:
+    if age<=45:
+        print("Eligible for LIC insurance")
+    else:
+        print("Not elisible for LIC insurance")
+    
