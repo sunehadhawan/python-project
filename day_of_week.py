0e151a895c0e445eab8797626d0f7b1a7bdd83ea
sunehadhawan <anmoldhawan090@gmail.com>
2026-07-06 23:41:51 -0700

    Add files via upload

---
+++ day_of_week.py
@@ -0,0 +1,15 @@
+day=int(input("enter the day of week (1-7): "))
+if day==1:
+    print("monday")
+elif day==2:
+    print("tuseday")
+elif day==3:
+    print("wednesday")
+elif day==4:
+    print("thursday")
+elif day==5:
+    print("friday")
+elif day==6:
+    print("saturday")
+elif day==7:
+    print("sunday")
